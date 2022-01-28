464353f270ed48be0c26425e07f32f4df1588d05
João Breno <joao.breno.rodrigues07@aluno.ifce.edu.br>
2022-01-28 13:48:31 -0300

    tentativa de determinar melhor regiao de corte automaticamente

--- cutter.py
+++ cutter.py
@@ -5,6 +5,8 @@ import argparse
 import shutil
 import os
 
+from utils import Rectangle, find_highway_region
+
 
 def get_txt_name(image_name):
     '''
@@ -16,156 +18,157 @@ def get_txt_name(image_name):
     return '.'.join(split)
 
 
-def get_txt_path(image_path, input_path):
+def get_txt_path(image_path):
     '''
     Retorna o caminho original para o .txt correspondente uma dada imagem
     '''
 
     image_name = os.path.basename(image_path)
     txt_name = get_txt_name(image_name)
-    return os.path.join(input_path, txt_name)
+    return os.path.join(os.path.dirname(image_path), txt_name)
 
 
-def get_cropped_image_and_txt_path(image_path, input_path, output_path, replace):
+def get_cropped_image_and_txt_path(image_path, output_path, suffix_text):
     '''
-    Retorna o caminho onde a imagem cortada será salva
+    Retorna o caminho onde a imagem cortada e o txt serao salva
     '''
 
     image_name = os.path.basename(image_path)
     txt_name = get_txt_name(image_name)
-
-    if output_path:
-        return (os.path.join(output_path, image_name), os.path.join(output_path, txt_name),)
-
-    if replace:
-        return (os.path.join(input_path, image_name), os.path.join(input_path, txt_name),)
-
     split_image_name = image_name.split('.')
     split_txt_name = txt_name.split('.')
+    cropped_image_name = '.'.join(
+        split_image_name[:-1]) + suffix_text + '.' + split_image_name[-1]
+    cropped_txt_name = '.'.join(
+        split_txt_name[:-1]) + suffix_text + '.' + split_txt_name[-1]
 
-    return (os.path.join(input_path,
-                         '.'.join(split_image_name[:-1]) + '_cropped.' + split_image_name[-1]),
-            os.path.join(input_path,
-                         '.'.join(split_txt_name[:-1]) + '_cropped.' + split_txt_name[-1]),)
+    return (os.path.join(output_path, cropped_image_name),
+            os.path.join(output_path, cropped_txt_name),)
 
 
-def cut(left_x, top_y, right_x, bottom_y, input_path, output_path=None, replace=False):
+def cropped_image_rectangle_from_highway_region(highway_region: Rectangle, width: int, height: int,
+                                                original_image_size):
     '''
-    Corta imagens
-
-    Parameters
-    ----------
-        - 'left_x' : Coordenada x da esquerda.
-        - 'top_y' : Coordenada y de cima.
-        - 'right_x' : Coordenada x da direita.
-        - 'bottom_y' : Coordenada y de baixo.
-        - 'input_path' : Caminho para as imagens
-        - 'output_path' : (Opcional) O caminho para as imagens cortadas
-        - 'replace' : Se as imagens devem ser substituidas. Usado apenas se output_path não é definido
+    Define a regiao de corte, dada a regiao da rodovia
     '''
 
-    paths = [f for f in os.listdir(input_path)]
+    hood_approximate_height = 240
+    x_min = max(0, highway_region.centre_x() - (width // 2))
+    x_max = x_min + width
+    y_min = \
+        highway_region.y_min \
+        if highway_region.height() >= height \
+        else highway_region.y_max - height
+    y_min = max(0, y_min)
+    y_max = y_min + height
 
-    if output_path:
-        if not os.path.exists(output_path):
-            os.mkdir(output_path)
+    if y_max < (original_image_size[1] - hood_approximate_height):
+        y_max = original_image_size[1] - hood_approximate_height
+        y_min = y_max - height
 
-        classes_file_name = "classes.txt"
-        if classes_file_name in paths:
-            paths.remove(classes_file_name)
-            shutil.copyfile(os.path.join(input_path, classes_file_name),
-                            os.path.join(output_path, classes_file_name))
+    return Rectangle(x_min, y_min, x_max, y_max)
 
-    images_paths = [os.path.join(input_path, i)
-                    for i in paths
-                    if i.endswith(('.JPG', '.jpg', '.jpeg', '.JPEG', '.png',))]
 
-    for image_path in images_paths:
-        txt_path = get_txt_path(image_path, input_path)
+def cut(cropped_image_rectangle: Rectangle, image: Image, output_image_path):
+    '''
+    Corta uma imagem
+    '''
 
-        try:
-            labels = pd.read_csv(txt_path, sep=' ',
-                                 names=['class', 'x', 'y', 'w', 'h'])
-        except:
-            print(f"(Aviso): Nao foi possivel abrir {txt_path}")
-            continue
+    x_min, y_min, x_max, y_max = cropped_image_rectangle.describe()
 
-        image = Image.open(image_path)
+    cropped_image = image.crop((x_min, y_min, x_max, y_max,))
+    cropped_image.save(output_image_path)
 
-        width, height = image.size
 
-        _x1 = max(0, min(left_x, width))
-        _y1 = max(0, min(top_y, height))
-        _x2 = max(0, min(right_x, width))
-        _y2 = max(0, min(bottom_y, height))
+def generate_txt(input_txt_path: str, output_txt_path: str, cropped_image_rectangle: Rectangle,
+                 original_image_size):
+    '''
+    Gera o txt da imagem cortada, com base no txt da imagem original
+    '''
 
-        cropped_image_width = _x2 - _x1
-        cropped_image_height = _y2 - _y1
+    try:
+        labels = pd.read_csv(input_txt_path, sep=' ',
+                             names=['class', 'x', 'y', 'w', 'h'])
+    except:
+        print(f"(Aviso): Nao foi possivel abrir {input_txt_path}")
+        return False
 
-        cropped_image = image.crop((_x1, _y1, _x2, _y2,))
-        cropped_image_path, new_txt_path = get_cropped_image_and_txt_path(
-            image_path, input_path, output_path, replace)
+    original_image_width, original_image_height = original_image_size
 
-        # reescalando valores
-        labels[['x', 'w']] = labels[['x', 'w']] * width
-        labels[['y', 'h']] = labels[['y', 'h']] * height
+    # reescalando valores
+    labels[['x', 'w']] = labels[['x', 'w']] * original_image_width
+    labels[['y', 'h']] = labels[['y', 'h']] * original_image_height
 
-        boxes = []
+    cropped_image_width = cropped_image_rectangle.x_max - cropped_image_rectangle.x_min
+    cropped_image_height = cropped_image_rectangle.y_max - cropped_image_rectangle.y_min
+    cropped_image_pol = Polygon([
+        (cropped_image_rectangle.x_min, cropped_image_rectangle.y_min),
+        (cropped_image_rectangle.x_max, cropped_image_rectangle.y_min),
+        (cropped_image_rectangle.x_max, cropped_image_rectangle.y_max),
+        (cropped_image_rectangle.x_min, cropped_image_rectangle.y_max)])
 
-        for row in labels.iterrows():
-            x1 = row[1]['x'] - row[1]['w']/2
-            y1 = row[1]['y'] - row[1]['h']/2
-            x2 = row[1]['x'] + row[1]['w']/2
-            y2 = row[1]['y'] + row[1]['h']/2
+    txt_rows = []
+    boxes = []
 
-            boxes.append((int(row[1]['class']), Polygon(
-                [(x1, y1), (x2, y1), (x2, y2), (x1, y2)])))
+    for row in labels.iterrows():
+        x1 = row[1]['x'] - row[1]['w']/2
+        y1 = row[1]['y'] - row[1]['h']/2
+        x2 = row[1]['x'] + row[1]['w']/2
+        y2 = row[1]['y'] + row[1]['h']/2
 
-        cropped_image_pol = Polygon(
-            [(_x1, _y1), (_x2, _y1), (_x2, _y2), (_x1, _y2)])
-        txt_rows = []
+        boxes.append((int(row[1]['class']), Polygon(
+            [(x1, y1), (x2, y1), (x2, y2), (x1, y2)])))
 
-        for box in boxes:
-            if cropped_image_pol.intersects(box[1]):
-                inter = cropped_image_pol.intersection(box[1])
-                new_box = inter.envelope
+    for box in boxes:
+        if cropped_image_pol.intersects(box[1]):
+            inter = cropped_image_pol.intersection(box[1])
+            new_box = inter.envelope
 
-                # obtendo o novo centro do bounding box
-                centre = new_box.centroid
+            # obtendo o novo centro do bounding box
+            centre = new_box.centroid
 
-                # obtendo as coordenadas dos vertices do poligono
-                x, y = new_box.exterior.coords.xy
+            # obtendo as coordenadas dos vertices do poligono
+            x, y = new_box.exterior.coords.xy
 
-                # YOLO normalizacao para a nova largura e altura
-                new_width = (max(x) - min(x)) / cropped_image_width
-                new_height = (max(y) - min(y)) / cropped_image_height
+            # YOLO normalizacao para a nova largura e altura
+            new_width = (max(x) - min(x)) / cropped_image_width
+            new_height = (max(y) - min(y)) / cropped_image_height
 
-                # YOLO normalizacao para as coordenadas do novo centro
-                new_x = (centre.coords.xy[0][0] - left_x) / cropped_image_width
-                new_y = (centre.coords.xy[1][0] - top_y) / cropped_image_height
+            # YOLO normalizacao para as coordenadas do novo centro
+            new_x = (
+                centre.coords.xy[0][0] - cropped_image_rectangle.x_min) / cropped_image_width
+            new_y = (
+                centre.coords.xy[1][0] - cropped_image_rectangle.y_min) / cropped_image_height
 
-                txt_rows.append(
-                    [box[0], new_x, new_y, new_width, new_height])
+            txt_rows.append([box[0], new_x, new_y, new_width, new_height])
 
-        if len(txt_rows) > 0:
-            txts_df = pd.DataFrame(txt_rows,
-                                   columns=['class', 'x', 'y', 'w', 'h'])
-            txts_df.to_csv(new_txt_path, sep=' ', index=False,
-                           header=False, float_format='%.6f')
-            cropped_image.save(cropped_image_path)
+    if len(txt_rows) > 0:
+        txts_df = pd.DataFrame(txt_rows,
+                               columns=['class', 'x', 'y', 'w', 'h'])
+        txts_df.to_csv(output_txt_path, sep=' ', index=False,
+                       header=False, float_format='%.6f')
+
+        return True
+
+    print(f"(Aviso): Sem conteudo para {input_txt_path}")
+    print(cropped_image_rectangle)
+    print()
+
+    return False
+
+
+def create_dir_if_not_exists(dir):
+    if not os.path.exists(dir):
+        os.mkdir(dir)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-left_x", type=int, default=410,
-                        help="Coordenada x da esquerda. Dafault: 410")
-    parser.add_argument("-right_x", type=int, default=3580,
-                        help="Coordenada x da direita. Dafault: 3580")
-    parser.add_argument("-top_y", type=int, default=1080,
-                        help="Coordenada y de cima. Dafault: 1080")
-    parser.add_argument("-bottom_y", type=int, default=1950,
-                        help="Coordenada y de baixo. Dafault: 1950")
+    parser.add_argument("-width", type=int, default=2100,
+                        help="A largura das novas imagens. Dafault: 2100")
+    parser.add_argument("-height", type=int, default=800,
+                        help="A altura das novas imagens. Dafault: 800")
     parser.add_argument("-input_path", required=True,
                         help="Caminho para as imagens")
     parser.add_argument("-output_path", default=None,
@@ -175,5 +178,60 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    cut(args.left_x, args.top_y, args.right_x, args.bottom_y,
-        args.input_path, args.output_path, args.replace != 0)
+    paths = [f for f in os.listdir(args.input_path)]
+    suffix_output_files_text = ''
+    output_path = args.input_path
+    logs_path_created = False
+    logs_path = "./logs"
+
+    if args.output_path and args.output_path != args.input_path:
+        output_path = args.output_path
+
+        create_dir_if_not_exists(args.output_path)
+
+        classes_file_name = "classes.txt"
+        if classes_file_name in paths:
+            paths.remove(classes_file_name)
+            shutil.copyfile(os.path.join(args.input_path, classes_file_name),
+                            os.path.join(args.output_path, classes_file_name))
+    elif args.replace != 0:
+        suffix_output_files_text = '_cropped'
+
+    images_paths = [os.path.join(args.input_path, i)
+                    for i in paths
+                    if i.endswith(('.JPG', '.jpg', '.jpeg', '.JPEG', '.png',))]
+
+    for image_path in images_paths:
+
+        highway_region = find_highway_region(image_path)
+
+        if highway_region:
+
+            image = Image.open(image_path)
+            if args.width > image.size[0] or args.height > image.size[1]:
+                print('(Aviso): A largura e/ou largura solicitada(s)'
+                      f' excedem o tamanho da imagem {image_path}')
+                continue
+
+            output_image_path, output_txt_path = get_cropped_image_and_txt_path(
+                image_path, output_path, suffix_output_files_text)
+
+            cropped_image_rectangle = cropped_image_rectangle_from_highway_region(
+                highway_region, args.width, args.height, image.size)
+
+            input_txt_path = get_txt_path(image_path)
+            txt_saved = generate_txt(input_txt_path, output_txt_path,
+                                     cropped_image_rectangle, image.size)
+
+            if txt_saved:
+                cut(cropped_image_rectangle, image, output_image_path)
+            else:
+                if not logs_path_created:
+                    create_dir_if_not_exists(logs_path)
+                    logs_path_created = True
+
+                cut(cropped_image_rectangle, image,
+                    os.path.join(logs_path, os.path.basename(output_image_path)))
+
+        else:
+            print(f'(Aviso): Rodovia não identificada em {image_path}')
---
+++ utils.py
@@ -0,0 +1,105 @@
+# Importing needed library
+import cv2
+
+
+class Rectangle:
+    def __init__(self, x_min: int, y_min: int, x_max: int, y_max: int) -> None:
+        self.x_min = x_min
+        self.y_min = y_min
+        self.x_max = x_max
+        self.y_max = y_max
+
+    def describe(self):
+        return (self.x_min, self.y_min, self.x_max, self.y_max,)
+
+    def centre_x(self):
+        return (self.x_min + self.x_max) // 2
+
+    def centre_y(self):
+        return (self.y_min + self.y_max) // 2
+
+    def height(self):
+        return self.y_max - self.y_min
+
+    def width(self):
+        return self.x_max - self.x_min
+
+    def __str__(self) -> str:
+        return f'{self.x_min} {self.y_min} {self.x_max} {self.y_max}'
+
+
+def get_contour(mask):
+    v = cv2.__version__.split('.')[0]
+
+    # In OpenCV version 3 function cv2.findContours() returns three parameters:
+    # modified image, found Contours and hierarchy
+    # All found Contours from current frame are stored in the list
+    # Each individual Contour is a Numpy array of(x, y) coordinates
+    # of the boundary points of the Object
+    # We are interested only in Contours
+
+    # Checking if OpenCV version 3 is used
+    if v == '3':
+        _, contours, _ = cv2.findContours(
+            mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    # In OpenCV version 4 function cv2.findContours() returns two parameters:
+    # found Contours and hierarchy
+    # All found Contours from current frame are stored in the list
+    # Each individual Contour is a Numpy array of(x, y) coordinates
+    # of the boundary points of the Object
+    # We are interested only in Contours
+
+    # Checking if OpenCV version 4 is used
+    else:
+        contours, _ = cv2.findContours(
+            mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    # Finding the biggest Contour by sorting from biggest to smallest
+    # contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    # if contours:
+    return contours
+
+
+def find_highway_region(image_path: str):
+    """
+    Detecting Object with chosen Colour Mask
+
+    Algorithm:
+    Reading RGB image --> Converting to HSV --> Implementing Mask -->
+    --> Finding Contour Points --> Extracting Rectangle Coordinates
+    """
+
+    # intervalos de valores para diferentes tonalidades de rodovia ja mapeadas
+    range_values = [
+        ((0, 0, 92), (184, 19, 144)),
+        ((103, 31, 0), (120, 70, 110)),
+        ((98, 0, 83), (138, 42, 168)),
+        ((94, 11, 55), (132, 46, 100)),
+        ((100, 12, 68), (126, 52, 112)),
+    ]
+
+    # Read local image
+    image_BGR = cv2.imread(image_path)
+
+    # Converting current image to HSV
+    image_HSV = cv2.cvtColor(image_BGR, cv2.COLOR_BGR2HSV)
+
+    masks = [
+        cv2.inRange(image_HSV, lowerb, upperb)
+        for lowerb, upperb in range_values
+    ]
+
+    contours = []
+    for mask in masks:
+        contours += get_contour(mask)
+
+    # Extracting Coordinates of the biggest Contour if any was found
+    if len(contours) > 0:
+        # Getting rectangle coordinates and spatial size from biggest Contour
+        # Function cv2.boundingRect() is used to get an approximate rectangle
+        # around the region of interest in the binary image after Contour was found
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)
+        (x_min, y_min, box_width, box_height) = cv2.boundingRect(contours[0])
+        return Rectangle(x_min, y_min, x_min + box_width, y_min + box_height)
